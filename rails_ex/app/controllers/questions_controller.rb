2d8fc3937e536082acfdfe955accd08ca9901c9a
thruc <horiuchi.github@gmail.com>
2021-11-02 23:48:11 +0900

    新規質問投稿画面追加

--- rails_ex/app/controllers/questions_controller.rb
+++ rails_ex/app/controllers/questions_controller.rb
@@ -5,7 +5,29 @@ class QuestionsController < ApplicationController
 
   def show; end
 
-  def new; end
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to root_path, notice: 'Success!'
+    else
+      flash[:alert] = 'Save error!'
+      render :new
+    end
+  end
+
+  def new
+    @question = Question.new
+  end
 
   def edit; end
+
+  private
+
+  def set_question
+    @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:name, :title, :content)
+  end
 end
